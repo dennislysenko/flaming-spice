7a92be02bd75b6dc57be2a411ba8ee66c1ded71e
Andriy Katkov <andriy.katkov@gmail.com>
2014-01-25 17:52:00 -0500

    popupstuff

--- Assets/Custom Scripts/TutorialCube.js
+++ Assets/Custom Scripts/TutorialCube.js
@@ -1,10 +1,14 @@
 ﻿#pragma strict
 
-function Start () {
-
-}
-
 var inCube : boolean = false;
+var textManager : GameObject;;
+
+function Start () {
+	textManager = GameObject.FindGameObjectWithTag("TextManager");
+	textManager.guiText.text = "Welcome to Alterego!";
+	yield WaitForSeconds(3);
+	textManager.guiText.text = "";
+}
 
 function Update () {
 
@@ -14,14 +18,20 @@ function Update () {
 
 }
 
+function OnGUI() {
+
+}
+
 function OnTriggerEnter (col : Collider) {
 	if (col.gameObject.name == "Alterego Player") {
 		inCube = true;
+		textManager.guiText.text = "Press E to enter the level...";
 	}
 }
 
 function OnTriggerExit (col : Collider) {
 	if (col.gameObject.name == "Alterego Player") {
 		inCube = false; 
+		textManager.guiText.text = "";
 	}
 }
