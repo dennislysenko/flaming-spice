5d4c15c31833202ddab4ee69f88388c1ee01b9c1
Andriy Katkov <andriy.katkov@gmail.com>
2014-01-25 15:03:16 -0500

    no merge or pl0x:

---
+++ Assets/LevelSelect.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnGUI () {
+
+	if (GUI.Button(new Rect(162, 290, 60, 50), "Tutorial")) {
+		Debug.Log("I am clickzor");
+		Application.LoadLevel("map_tutorial");
+	}
+
+}
