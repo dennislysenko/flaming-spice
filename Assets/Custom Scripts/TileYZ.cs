c91e17b5198747e350e51a2a28528bc858eca453
Tony Reiter <treiter@vt.edu>
2014-01-26 11:31:56 -0500

    textures on map_intventor1

--- Assets/Custom Scripts/TileYZ.cs
+++ Assets/Custom Scripts/TileYZ.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class TileYZ : MonoBehaviour {
 
 	public float ScaleToTiles = 0.667f;
-	
+
 	void Start () {
 		Vector2 v = renderer.material.mainTextureScale;
 		Debug.Log (transform.lossyScale);
