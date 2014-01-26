35be9574c9e2e11a777c4a8f1a1212b204f6724c
Tony Reiter <treiter@vt.edu>
2014-01-26 11:08:36 -0500

    textures for tutorials

--- Assets/Custom Scripts/TileYZ.cs
+++ Assets/Custom Scripts/TileYZ.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class TileYZ : MonoBehaviour {
 
-	float ScaleToTiles = 0.667f;
+	public float ScaleToTiles = 0.667f;
 	
 	void Start () {
 		Vector2 v = renderer.material.mainTextureScale;
 		Debug.Log (transform.lossyScale);
-		v.x = transform.lossyScale.y*ScaleToTiles;
-		v.y = transform.lossyScale.z*ScaleToTiles;
+		v.x = transform.lossyScale.z*ScaleToTiles;
+		v.y = transform.lossyScale.y*ScaleToTiles;
 		renderer.material.mainTextureScale = v;
 	}
 }
