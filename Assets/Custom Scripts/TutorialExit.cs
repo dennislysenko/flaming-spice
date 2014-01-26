76187e2db0aad41e9d57858ab5113e6ac6421ece
Andriy Katkov <andriy.katkov@gmail.com>
2014-01-26 00:03:47 -0500

    hsahfsd

---
+++ Assets/Custom Scripts/TutorialExit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine;
+using System.Collections;
+
+public class TutorialExit : MonoBehaviour {
+	
+	public string level;
+	private bool firstWin = true;
+	
+	void OnTriggerEnter (Collider col) {
+		if (col.gameObject.name == "Alterego Player") {
+			if (firstWin) {
+				firstWin = false;
+				Application.LoadLevel ("map_tutorial_secondtime");
+			} else {
+				PersitentLevelManager.LevelCompleted(level);
+				Application.LoadLevel("LevelSelect");
+			}
+		}
+	}
+}
